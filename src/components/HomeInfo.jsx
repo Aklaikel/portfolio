279e4e8784949a95ca08bdfc702c6d8bcd79d510
aklaikel <anas.klaikel95@gmail.com>
2023-11-16 04:50:35 +0100

    add the entire home

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import arrow from '../assets/icons/arrow.svg'
 
+const InfoBox = ( {text, link, btn} ) => {
+    return (
+        <div className='info-box'>
+            <p className='font-medium sm:text-xl text-center'>{text}</p>
+            <Link to={(link)} className='neo-brutalism-white neo-btn'>
+                {btn}
+                <img src={arrow} className='w-4 h-4 object-contain'/>
+            </Link>
+        </div>
+    )
+}
 const renderContent = {
     1: (
         <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue
@@ -10,26 +22,28 @@ const renderContent = {
         </h1>
     ),
     2: (
-       <InfoBox/>
+        <InfoBox
+        text="Let's work together and build something great!"
+          link='/contact'
+         btn="Get In Touch"
+        />
     ),
     3: (
-        <h1>3</h1>
+        <InfoBox
+        text="Done multiple projects using different technologies and frameworks."
+          link='/projects'
+         btn="My Projects"
+        />
     ),
     4: (
-        <h1>4</h1>
+        <InfoBox
+        text="I am a software engineer with a passion for building things that live on the internet. I develop exceptional websites and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends."
+          link='/about'
+         btn="Learn More"
+        />
     ),
 }
 
-const InfoBox = ( {text, link, btn} ) => {
-    return (
-        <div className='info-box'>
-            {text}
-            <Link to={(link)}>
-                {btn}
-            </Link>
-        </div>
-    )
-}
 
 function HomeInfo({currentStage}) {
   return (
