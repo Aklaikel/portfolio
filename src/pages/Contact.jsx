cd2d90208627d8a72e3f32ac9c9bf7ef6945e8dd
aklaikel <anas.klaikel95@gmail.com>
2023-11-19 14:30:54 +0100

    add email sender (emailjs)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 function Contact() {
   const formRef = useRef();
@@ -21,14 +22,31 @@ function Contact() {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    setTimeout(() => {
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: "Anass",
+        from_email: form.email,
+        to_email:'anas.klaike95@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_USER_ID
+    ).then((result) => {
+      console.log(result.text);
       setIsLoading(false);
       setForm({
         name: '',
         email: '',
         message: '',
-      })
-    }, 2000);
+      });
+      formRef.current.reset();
+    }
+    ).catch((err) => {
+      console.log(err.text);
+      setIsLoading(false);
+    });
   }
   return (
    <section className="relative flex lg:flex-row flex-col max-container">
